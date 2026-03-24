c1553168f4ddf9153b2b5a60211441b0237e852d
Hasan Malik <hasanimranmalik245@gmail.com>
2026-03-23 23:16:00 -0400

    Initial Commit

---
+++ CricPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CricPulse
+//
+//  Created by Hasan Malik on 2026-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CricPulse/CricPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricPulseApp.swift
+//  CricPulse
+//
+//  Created by Hasan Malik on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct CricPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
